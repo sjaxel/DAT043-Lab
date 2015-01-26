220d29f633980c5fc4ab24632c2acec423cb7eab
sjaxel <axel.sjogren.holtz@gmail.com>
2015-01-26 11:25:20 +0100

    Added lab2 dir?

--- Lab1/Date.java
+++ Lab1/Date.java
@@ -33,61 +33,105 @@ public class Date {
 	
 	    
 	public static void main (String[] arg) {
+		// Main loop
 		while (true) {
-		String q = JOptionPane.showInputDialog("Skriv i formen åååå-mm-dd");
-        if (q != null) {
-            
-            String syear  = q.substring(0, 4);
-            String smonth = q.substring(5, 7);
-            String sday = q.substring(8, 10);
-            
-            int y = Integer.parseInt(syear);
-            int m = Integer.parseInt(smonth);
-            int d = Integer.parseInt(sday);
-            int[] listOfMonth = {31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31};
-            
-            int totaldays = 0;
-            int i = y;
-            while (i > 1754) {
-                Date iyear = new Date(i-1, 01, 01);
-                totaldays = totaldays + iyear.tdays;
-                i = i - 1;
-            }
-            i = m;
-            while (i > 1) {
-                totaldays = totaldays + listOfMonth[i-2];
-                i = i - 1;
-            }
-            totaldays = totaldays + d;
-            
-            Date userdate = new Date(y,m,d);
-            if ((userdate.leapyear == true) && (userdate.month > 2)) {
-                totaldays = totaldays + 1;
-            }
-            System.out.println(totaldays);
-            if (totaldays%7 == 0) {
-                   System.out.println("Det är en Måndag");
-                }
-                else if (totaldays%7 == 1 ) {
-                   System.out.println("Det är en Tisdag");
-                }
-                else if (totaldays%7 == 2 ) {
-                   System.out.println("Det är en Onsdag");
-                }
-                else if (totaldays%7 == 3 ) {
-                   System.out.println("Det är en Torsdag");
-                }
-                else if (totaldays%7 == 4 ) {
-                   System.out.println("Det är en Fredag");
-                }
-                else if (totaldays%7 == 5 ) {
-                   System.out.println("Det är en Lördag");
-                }
-                else if (totaldays%7 == 6 ) {
-                   System.out.println("Det är en Söndag");
-                }
-           }
-           else break;
+			Input userinput = new Input();
+			if (userinput.isValid()) {
+				int[] listOfMonth = {31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31};
+				
+				int totaldays = 0;
+				int i = userinput.getDate("y");
+				while (i > 1754) {
+					Date iyear = new Date(i-1, 01, 01);
+					totaldays = totaldays + iyear.tdays;
+					i = i - 1;
+				}
+				i = userinput.getDate("m");
+				while (i > 1) {
+					totaldays = totaldays + listOfMonth[i-2];
+					i = i - 1;
+				}
+				totaldays = totaldays + userinput.getDate("d");
+				
+				Date userdate = new Date(userinput.getDate("y"),
+				userinput.getDate("m"),userinput.getDate("d"));
+				if ((userdate.leapyear == true) && (userdate.month > 2)) {
+					totaldays = totaldays + 1;
+				}
+				
+				System.out.println(totaldays);
+				if (totaldays%7 == 0) {
+					System.out.println("Det är en Måndag");
+					}
+					else if (totaldays%7 == 1 ) {
+					System.out.println("Det är en Tisdag");
+					}
+					else if (totaldays%7 == 2 ) {
+					System.out.println("Det är en Onsdag");
+					}
+					else if (totaldays%7 == 3 ) {
+					System.out.println("Det är en Torsdag");
+					}
+					else if (totaldays%7 == 4 ) {
+					System.out.println("Det är en Fredag");
+					}
+					else if (totaldays%7 == 5 ) {
+					System.out.println("Det är en Lördag");
+					}
+					else if (totaldays%7 == 6 ) {
+					System.out.println("Det är en Söndag");
+					}
+			}
+			else break;
         }
 	}
 }
+
+class Input {
+	private int y, m, d;
+	String q;
+	Input() {
+		q = JOptionPane.showInputDialog("Skriv i formen åååå-mm-dd");
+		String syear  = q.substring(0, 4);
+		String smonth = q.substring(5, 7);
+		String sday = q.substring(8, 10);
+				
+		y = Integer.parseInt(syear);
+		m = Integer.parseInt(smonth);
+		d = Integer.parseInt(sday);
+	}
+	public boolean isValid() {
+		if (q != null) {
+			return true;
+		}
+		else {
+			return false;
+		}
+	}
+	public int getDate(String t) {
+		if (t == "y")
+			return y;
+		else if (t == "m")
+			return m;
+		else if (t == "d")
+			return d;
+		return 0;
+	}
+	
+		
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
